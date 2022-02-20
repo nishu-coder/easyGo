7c308460db08a34b251a880cb9891b0abf7b8abc
nishucoder <nishant.jaiswal@cdgi.edu.in>
2022-02-20 16:36:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/easygo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.easygo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
